d0bab6e8517b394a6af69e99124fb930d4d3ec8a
Sergii Bilyi <kartanisoid@gmail.com>
2019-09-16 00:45:59 +0300

    implemented O(n) solution using stack

---
+++ src/com/sbilyi/solution/moderate/BasicModerateSecond.java
@@ -0,0 +1,45 @@
+package com.sbilyi.solution.moderate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class BasicModerateSecond implements ModerateSecond {
+
+    @Override
+    public String[] solve(int numberOfParentheses) {
+        if (numberOfParentheses <= 0) {
+            return new String[]{};
+        } else if (numberOfParentheses == 1) {
+            return new String[]{"()"};
+        }
+
+        String[] result = solve(numberOfParentheses - 1);
+        Stack<String> prev = new Stack<>();
+        List<String> actual = new ArrayList<>();
+
+        for (int i = result.length - 1; i >= 0; i--) {
+            prev.push(result[i]);
+        }
+
+        for(; !prev.empty(); ) {
+            String str = prev.pop();
+            actual.add("(" + str + ")");
+            actual.add("()" + str);
+            if(str.contains("((")) {
+                actual.add(str + "()");
+            }
+        }
+
+        return actual.toArray(new String[0]);
+    }
+
+    private String[] ensureCapacity(String[] array, int input) {
+        if (array.length < input) {
+            return Arrays.copyOf(array, input);
+        } else {
+            return array;
+        }
+    }
+}
